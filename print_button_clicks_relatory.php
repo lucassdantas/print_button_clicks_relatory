8590ee1a04d8a294d89264ade982c277d6f1efe0
Lucas Dantas <lucasdantasprogramador@gmail.com>
2023-12-30 10:27:05 -0300

    first commit

---
+++ print_button_clicks_relatory.php
@@ -0,0 +1,13 @@
+<?php 
+/**
+ * Plugin Name: Print Button Clicks Relatory
+ * Description: Add the command print_relatory to wp-cli to print button click relatory
+ * Plugin URI:  https://github.com/lucassdantas/print_button_clicks_relatory.git
+ * Version:     1.0.0
+ * Author:      Lucas Dantas
+ * Author URI:  linkedin.com/in/lucas-de-sousa-dantas/
+ */
+
+ if ( ! defined( 'ABSPATH' ) ||  !function_exists('add_action')) {
+	exit;
+}
