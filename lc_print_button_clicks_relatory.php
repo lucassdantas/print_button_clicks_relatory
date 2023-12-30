6af127d4b0a8d325d7cf5715b94bc7fa67b493d2
Lucas Dantas <lucasdantasprogramador@gmail.com>
2023-12-30 11:19:45 -0300

    add temp error message

--- lc_print_button_clicks_relatory.php
+++ lc_print_button_clicks_relatory.php
@@ -14,11 +14,26 @@
 
 register_activation_hook( __FILE__, 'onActivate' );
 function onActivate(){
-	if ( ! is_plugin_active( 'lc_button_click_register/lc_button_click_register.php' ) && current_user_can( 'activate_plugins' ) ) {
-        wp_die(
-            'Para utilizar este plugin, é necessário ativar o plugin "<a href="https://github.com/lucassdantas/wp_lcButtonClickRegister" target="_blank">lc_button_click_register</a>" <br><a href="' 
-            .admin_url( 'plugins.php' ) 
-            .'">&laquo; Voltar para plugins</a>'
-        );
+    if ( 
+        is_admin() 
+        && current_user_can( 'activate_plugins' ) 
+        && !is_plugin_active( 'lc_button_click_register/lc_button_click_register.php' ) 
+    ) {
+        add_action( 'admin_notices', function(){
+
+            lc_print_button_click_relatory_notice_error($error_message);
+        });
+        deactivate_plugins( plugin_basename( __FILE__ ) ); 
+        if (isset( $_GET['activate'])) unset( $_GET['activate'] );
+        $error_message = 'Para utilizar este plugin, é necessário ativar o plugin <a href="https://github.com/lucassdantas/wp_lcButtonClickRegister" target="_blank">"lc_button_click_register"</a> <br><a href="' . admin_url( 'plugins.php' ) . '">&laquo; Voltar aos plugins</a>';
+        wp_die($error_message);
     }
+	
+}
+function lc_print_button_click_relatory_notice_error($message){
+?>
+    <div class="error">
+        <p><?php $message ?></p>
+    </div>
+<?php
 }
